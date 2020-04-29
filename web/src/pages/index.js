3d7550d198bc7cea4e1c9d97bac0cff2e50ae222
amrut <48245238+akulk5@users.noreply.github.com>
2020-04-29 13:22:29 +0100

    Update index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -99,7 +99,7 @@ const IndexPage = props => {
         <h1 hidden>Welcome to {site.title}</h1>
         {postNodes && (
           <BlogPostPreviewList
-            title='Latest posts'
+            title='Latest posts from Amrut'
             nodes={postNodes}
             browseMoreHref='/archive/'
           />
